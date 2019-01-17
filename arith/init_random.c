904c27b9c88db8082163ea0a9b04ae3064f5722f
wellsaid <benedetto.girgenti@protonmail.com>
2019-01-17 17:46:55 +0100

    Modified init_random to use contiki RNG functions

--- arith/init_random.c
+++ arith/init_random.c
@@ -2,17 +2,20 @@
 #include <stdint.h> // for intptr_t
 #include <stdlib.h>
 #include <gmp.h>
+#include <time.h>
+
+#include <os/lib/random.h>
+
 #include "pbc_utils.h"
 #include "pbc_random.h"
 
-void pbc_init_random(void) {
-  FILE *fp;
-  fp = fopen("/dev/urandom", "rb");
-  if (!fp) {
-    pbc_warn("could not open /dev/urandom, using deterministic random number generator");
-    pbc_random_set_deterministic(0);
-  } else {
-    pbc_random_set_file("/dev/urandom");
-    fclose(fp);
-  }
+void contiki_randfunc(mpz_t num, mpz_t limit, void* data){
+	uint32_t n = random_rand();
+	mpz_init_set_ui(num, n);
 }
+
+void pbc_init_random(void) {
+	random_init(time(NULL));
+	pbc_random_set_function(contiki_randfunc, NULL);
+}
+
