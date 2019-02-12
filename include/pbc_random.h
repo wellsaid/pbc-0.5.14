0664c8914ce160cb0bc57a89e94c47485df6d735
wellsaid <benedetto.girgenti@protonmail.com>
2019-02-12 16:32:20 +0100

    Removed not used random function in contiki

--- include/pbc_random.h
+++ include/pbc_random.h
@@ -9,22 +9,6 @@ Initializes the pbc random moduile
 void pbc_random_init(void);
 
 /*@manual pbcrandom
-Sets 'filename' as a source of random bytes. For example,
-on Linux one might use `/dev/random`.
-*/
-void pbc_random_set_file(char *filename);
-
-/*@manual pbcrandom
-Uses a determinstic random number generator, seeded with 'seed'.
-*/
-void pbc_random_set_deterministic(unsigned int seed);
-
-/*@manual pbcrandom
-Uses given function as a random number generator.
-*/
-void pbc_random_set_function(void (*fun)(mpz_t, mpz_t, void *), void *data);
-
-/*@manual pbcrandom
 Selects a random 'z' that is less than 'limit'.
 */
 void pbc_mpz_random(mpz_t z, mpz_t limit);
